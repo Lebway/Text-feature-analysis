b5dc2456fc3d44c1e5f0d0e56a4b08f6bb712a40
Lebway <luojunyu@buaa.edu.cn>
2019-08-18 07:35:20 +0800

    add word count to the project

---
+++ wordcount.py
@@ -0,0 +1,16 @@
+# Run with: python wordcount.py my-file.ipynb
+import sys
+import json
+
+wordCount = 0
+filename = "Final Project.ipynb"
+with open(filename) as f:
+    notebookRaw = f.read()
+    notebookParsed = json.loads(notebookRaw)
+    for cell in notebookParsed['cells']:
+        if cell['cell_type'] == "markdown":
+            contents = ''.join(cell['source'])
+            words = contents.replace('#','').strip().split(' ')
+            wordCount += len(words)
+            print(words)
+print(filename + ": {} words".format(wordCount))
